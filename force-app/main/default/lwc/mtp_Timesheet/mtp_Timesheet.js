ff45a2e9d3bdf1f456470b68fc833a2ea206c6c1
Nitin2611 <bainitin2611@gmail.com>
2022-09-06 19:16:05 +0530

    6 Sep Before for loop

--- force-app/main/default/lwc/mtp_Timesheet/mtp_Timesheet.js
+++ force-app/main/default/lwc/mtp_Timesheet/mtp_Timesheet.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import mtp_Timesheet_bg_image from '@salesforce/resourceUrl/mtp_Timesheet_bg_image';
 import mtp_Approve_icon from '@salesforce/resourceUrl/mtp_Approve_icon';
 import mtp_Reject_icon from '@salesforce/resourceUrl/mtp_Reject_icon';
@@ -6,6 +6,7 @@ import mtp_Airplane_StartTime_icon from '@salesforce/resourceUrl/mtp_Airplane_St
 import mtp_Airplane_EndTime_icon from '@salesforce/resourceUrl/mtp_Airplane_EndTime_icon';
 import mtp_Calendar_icon from '@salesforce/resourceUrl/mtp_Calendar_icon';
 
+import getTimesheetData from '@salesforce/apex/mtp_TimesheetController.getTimesheetData';
 export default class Mtp_Timesheet extends LightningElement {
     bgImage = mtp_Timesheet_bg_image;                       // background-image of timesheet component
     approveIcon = mtp_Approve_icon;                         // approve icon for timesheet
@@ -14,5 +15,24 @@ export default class Mtp_Timesheet extends LightningElement {
     endTimeIcon = mtp_Airplane_EndTime_icon;                // end time (airplane) icon for timesheet
     calendarIcon = mtp_Calendar_icon;                       // calendar icon for timesheet
 
+    @track timesheetDataList = [];
 
+    connectedCallback() {
+        try {
+
+            getTimesheetData()
+                .then(result => {
+                    this.timesheetDataList = result;
+                    console.log("timesheetDataList ==>");
+                    console.log({ result });
+
+                })
+                .catch(error => {
+                    console.log({ error });
+                });
+
+        } catch (error) {
+            console.log({ error });
+        }
+    }
 }
